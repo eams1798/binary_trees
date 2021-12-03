232dbc6a8a91b871ee5d4a7a197a04862e6a12a8
estefanomisme <3401@holbertonschool.com>
2021-12-03 02:30:57 +0000

    binary trees - mandatory first part

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: a pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: the number of leaves, or 0 if the tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	;
+}
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ *
+ * Return: the sibling node, or NULL if:
+ * - the parent is NULL
+ *   - node is NULL
+ *   - node has no sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	;
+}
